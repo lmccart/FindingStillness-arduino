631457919946e5640d253244fa337b5754ca2f21
Kyle McDonald <kyle@kylemcdonald.net>
2015-03-15 11:33:40 -0700

    manually adding laurens changes after weird merge/rebase

--- FSBlinker_Programmer/FSBlinker_Programmer.ino
+++ FSBlinker_Programmer/FSBlinker_Programmer.ino
@@ -5,8 +5,8 @@
 #include "crc.h"
 
 const uint8_t DEFAULT_SENSITIVITY = 15; // 15-120
-const uint8_t DEFAULT_COUNTS = 2; // 1-5 (repeat 7-beat pattern)
-int heart_rate = 150; // rate 40-150
+const uint8_t DEFAULT_COUNTS = 5; // 1-5 (repeat 7-beat pattern)
+int heart_rate = 50; // rate 40-150
 
 IRsend irsend;
 
